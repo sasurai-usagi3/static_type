44c328017b39ee0c487712f99ac5a083e4e9d3ad
Takumu Uyama <t_uyama@r.recruit.co.jp>
2018-06-01 13:26:03 +0900

    feature: varメソッド作成

--- lib/static_type/functions.rb
+++ lib/static_type/functions.rb
@@ -1,5 +1,12 @@
 module StaticType
   def var(definitions)
-    puts definitions.inspect
+    definitions.each do |variable_name, variable_type|
+      define_method(variable_name) do
+        self.instance_variable_get('@' + variable_name.to_s)
+      end
+      define_method(variable_name.to_s + '=') do |value|
+        self.instance_variable_set('@' + variable_name.to_s, value)
+      end
+    end
   end
 end
